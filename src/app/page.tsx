83d1a0771f33551a14430b8f33f18886536927cc
OVECJOE <vohachor@gmail.com>
2025-05-27 20:04:00 +0100

    feat: completed v1 of the chatterbot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,108 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [theInput, setTheInput] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<
+    { role: "user" | "assistant"; content: string }[]
+  >([
+    {
+      role: "assistant",
+      content: "Yo, this ChatterBot! How can I help you today?",
+    },
+  ]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const callGetResponse = async () => {
+    setIsLoading(true);
+    const temp = messages;
+    temp.push({ role: "user", content: theInput });
+    setMessages(temp);
+    setTheInput("");
+
+    console.log("Calling OpenAI with input:", theInput);
+    try {
+      const response = await fetch("/api", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ messages }),
+      });
+
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(data.error || "Failed to fetch response from OpenAI");
+      }
+
+      const { output } = data;
+      console.log("OpenAI replied...", output.content);
+
+      setMessages((prev) => [...prev, output]);
+    } catch (error) {
+      setMessages((prev) => [
+        ...prev,
+        { role: "assistant", content: (error as Error).message },
+      ]);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const Submit = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (event.key !== "Enter" || event.shiftKey) {
+      return;
+    }
+
+    event.preventDefault();
+    callGetResponse();
+  };
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 py-5">
+      <h1 className="text-3xl font-sans">ChatterBot</h1>
+      <div className="flex h-[35rem] w-[40rem] flex-col items-center bg-gray-600 rounded-xl">
+        <div className="h-full flex flex-col gap-2 overflow-y-auto py-8 px-3 w-full">
+          {messages.map((e, i) => {
+            return (
+              <div
+                key={`${e.content}-${i}`}
+                className={`w-max max-w-[18rem] rounded-md px-4 py-3 h-min ${
+                  e.role === "assistant"
+                    ? "self-start bg-gray-200 text-gray-800"
+                    : "self-end bg-gray-800 text-gray-50"
+                } break-inside-auto wrap-break-word`}
+              >
+                <p className="text-sm">{e.content}</p>
+              </div>
+            );
+          })}
+
+          {isLoading && (
+            <div className="w-max max-w-[18rem] rounded-md px-4 py-3 h-min self-start bg-gray-200 text-gray-800">
+              <p className="text-sm">Thinking...</p>
+            </div>
+          )}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <div className="relative w-[80%] bottom-4 flex justify-center">
+          <textarea
+            value={theInput}
+            onChange={(e) => setTheInput(e.target.value)}
+            className="w-[85%] h-10 px-3 py-2 resize-none overflow-y-auto text-black bg-gray-300 rounded-l outline-none"
+            onKeyDown={Submit}
+            placeholder="Type your message here..."
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          <button
+            onClick={callGetResponse}
+            className="w-[20%] bg-background px-4 py-2 rounded-r"
+            disabled={isLoading}
+          >
+            {isLoading ? "Loading..." : "Send"}
+          </button>
+        </div>
+      </div>
+
+      <div></div>
+    </main>
   );
 }
